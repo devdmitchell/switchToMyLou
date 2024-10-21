c0be92ea14381bae18ef765e08b2550976a89ea1
devdmitchell <dmitchell.prsvr@gmail.com>
2024-10-21 10:29:23 -0500

    completed one switch statement

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
 /******************************/
 //Your Code Here 
 
-
+function dayOfTheWeek(day){
+  switch(day){
+    case 1 : {
+      return "Monday"
+      break
+    }
+  case 2 : {
+    return "Tuesday"
+    break
+  }
+  case 3 : {
+    return "Wednesday"
+    break
+  }
+  case 4 : {
+    return "Thursday"
+    break
+  }
+  case 5: {
+    return "Friday"
+  break
+}
+case 6 : {
+  return "Saturday"
+  break
+}
+case 7 : {
+  return "Sunday"
+  break
+}
+}}
+console.log(dayOfTheWeek(2))
 
 
 /****************************/
