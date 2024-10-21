2ad4dfdbdb58dc0db0d4a7e23d54d0f7d6355980
devdmitchell <dmitchell.prsvr@gmail.com>
2024-10-21 10:42:51 -0500

    completed second switch statement

--- main.js
+++ main.js
@@ -1,38 +1,91 @@
 /******************************/
 //Your Code Here 
 
-function dayOfTheWeek(day){
-  switch(day){
-    case 1 : {
-      return "Monday"
-      break
-    }
-  case 2 : {
-    return "Tuesday"
-    break
-  }
-  case 3 : {
-    return "Wednesday"
-    break
-  }
-  case 4 : {
-    return "Thursday"
-    break
-  }
-  case 5: {
-    return "Friday"
-  break
-}
-case 6 : {
-  return "Saturday"
-  break
-}
-case 7 : {
-  return "Sunday"
-  break
-}
-}}
-console.log(dayOfTheWeek(2))
+// function dayOfTheWeek(day){
+//   switch(day){
+//     case 1 : {
+//       return "Monday"
+//       break
+//     }
+//   case 2 : {
+//     return "Tuesday"
+//     break
+//   }
+//   case 3 : {
+//     return "Wednesday"
+//     break
+//   }
+//   case 4 : {
+//     return "Thursday"
+//     break
+//   }
+//   case 5: {
+//     return "Friday"
+//   break
+// }
+// case 6 : {
+//   return "Saturday"
+//   break
+// }
+// case 7 : {
+//   return "Sunday"
+//   break
+// }
+// }}
+// console.log(dayOfTheWeek(2))
+
+// function seasonIdentification(season){
+//   switch(season){
+//     case 1 : {
+//       return "Winter"
+//       break
+//     }
+//   case 2 : {
+//     return "Winter"
+//     break
+//   }
+//   case 3 : {
+//     return "Spring"
+//     break
+//   }
+//   case 4 : {
+//     return "Spring"
+//     break
+//   }
+//   case 5 : {
+//     return "Spring"
+//     break
+//   }
+//   case 6 : {
+//     return "Summer"
+//     break
+//   }
+//   case 7 : {
+//     return "Summer"
+//     break
+//   }
+//   case 8 : {
+//     return "Summer"
+//     break
+//   }
+//   case 9 : {
+//     return "Autumn"
+//     break
+//   }
+//   case 10 : {
+//     return "Autumn"
+//     break
+//   }
+//   case 11 : {
+//     return "Autumn"
+//     break
+//   }
+//   case 12 : {
+//     return "Winter"
+//     break
+//   }
+// }}
+// console.log(seasonIdentification(11))
 
 
 /****************************/
